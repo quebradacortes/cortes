4d0fef971ebbed44413aa57b84fb7fe3dbc4af13
quebradacortes <153042174+quebradacortes@users.noreply.github.com>
2023-12-05 22:49:29 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = () => navbar.classList.toggle("active");
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = () => navbar.classList.remove("active");
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+
+
+/**
+ * filter function
+ */
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all") {
+
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+
+    } else {
+
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+
+    }
+  }
+}
+
+addEventOnElem(filterBtns, "click", filter);
+
+
+
+function sendToWhatsapp(){
+  let number = "+551197760864"
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let date = document.getElementById('date').value;
+  let message = document.getElementById('message').value;
+
+
+  var url = "https://wa.me/+551197760864?text=Gostaria de agendar um horário" + number + "?text=Gostaria de agendar um horário"
+  
+
+  window.open(url, '_blank').focus();
+}
